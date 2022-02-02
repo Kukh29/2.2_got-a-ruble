c2f5381a255087dd3a508ff94389c6340073b527
kukh29 <kukh29@gmail.com>
2022-02-02 16:34:19 +0300

    v.1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // входные данные
+        int amount = 100; // начальная сумма счета
+        int replenishment = 1000; // сумма пополнения счета
+        int spent = 100; // 1 рубль начисляется за каждые полные 100 рублей пополнения
+
+        // логика
+        if (replenishment >= 1000) {
+            System.out.println("Количество бонусных рублей:");
+            System.out.println((replenishment / spent) + " рублей");
+        } else {
+            System.out.println("0 бонусных рублей:");
+        }
+        if (replenishment >= 1000) {
+        System.out.println("Итоговая сумма на счету клиента");
+        System.out.println((replenishment / spent+replenishment+amount) + " рублей");
+    } else {
+            System.out.println("Итоговая сумма на счету клиента");
+            System.out.println((replenishment+amount) + " рублей");
+        }
+        }
+}
