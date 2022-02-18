1a1fb6a48b0a50e19530ba4ad91ae4266a272e9d
kukh29 <kukh29@gmail.com>
2022-02-18 17:43:15 +0300

    v.6

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,22 @@ public class Main {
 
         // входные данные
         int amount = 100; // начальная сумма счета
-        int replenishment = 100; // сумма пополнения счета
+        int replenishment = 1000; // сумма пополнения счета
 
         // логика
-        int percent = 1;
+        int percent;
+        if (replenishment >= 1000) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+
         int bonus = replenishment * percent / 100;
 
-        if (replenishment >= 1000) {
-            bonus = replenishment * percent / 100;
-        } else {
-            bonus = 0;
-        }
-        System.out.println("Бонус");
-        System.out.println((bonus) + " рублей");
         System.out.println("Итоговая сумма на счету клиента");
         System.out.println((bonus + replenishment + amount) + " рублей");
+        System.out.println("Бонус");
+        System.out.println((bonus) + " рублей");
+
     }
 }
